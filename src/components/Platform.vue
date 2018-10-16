0cadff77dfa43c6307974bdff753779a87a269b0
cyko28 <scott.t.robertson@gmail.com>
2018-10-16 02:57:33 -0400

    removed methods

--- src/components/Platform.vue
+++ src/components/Platform.vue
@@ -1,44 +1,26 @@
 <template>
-    <div id="platform" class="wrapper" :style="{top: top + 'px', height: 517 + 'px'}">
+    <div id="platform" class="wrapper">
         <img ref="platformImg" src="../assets/platform.png" alt="The games platform" class="platform">
         <rocketMine></rocketMine>
     </div>
 </template>
 
 <script>
-import RocketMine from './RocketMine.vue'
 
 export default {
-    name: "Platform",
-    props: ['top'],
-    components: {
-        RocketMine
-    },
-    data() {
-        return {
-            platfromHeight: false
-        }
-    },
-    created () { // would work in 'ready', 'attached', etc.
-        window.addEventListener('load', () => {
-            this.platfromHeight = this.$refs.platformImg.clientHeight;
-        })
-    }
+    name: "Platform"
 }
 </script>
 
 <style>
 .wrapper {
-    position: absolute;
-    top: 0;
-    z-index: -1;
-    width: 100%;
-    overflow: hidden;
+    flex-basis: 100%;
+    flex-shrink: 1;
+    padding-right: 2rem;
+    padding-left: 2rem;
+    padding-top: 2rem;
 }
 .platform {
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    width: 1200px;
+    width: 100%;
 }
 </style>
