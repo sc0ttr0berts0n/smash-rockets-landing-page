b2f725431350e01b0cc286797a71993713e0b50e
cyko28 <scott.t.robertson@gmail.com>
2018-10-16 02:57:10 -0400

    removed text container, added logo

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,64 +1,64 @@
 <template>
     <div id="hero" class="hero-wrapper">
-        <div class="text-container">
-            <logo></logo>
-            <h2 class="subheadline">A physics-driven, 2d platform brawler</h2>
-            <a href="http://play.smashrockets.com/" class="button">Play the Demo</a>
-        </div>
-        <rocketTick></rocketTick>
+        <logo></logo>
+        <h2 class="subheadline">A physics-driven, 2d platform brawler</h2>
+        <a href="http://play.smashrockets.com/" class="button">Play the Demo</a>
     </div>
 </template>
 
 <script>
 import RocketTick from './RocketTick.vue';
+import Logo from './Logo.vue';
 
 export default {
   name: 'Hero',
   components: {
-      RocketTick
+      RocketTick, Logo
   }
 }
 </script>
 
 <style scoped>
 .hero-wrapper {
-    position: realtive;
-    z-index: 10;
-    margin: 2rem 2.5rem 0;
+    margin: 2rem 0.75rem 0;
     display: flex;
-    flex-basis: 1000px;
-    justify-content: space-between;
+    justify-content: center;
+    flex-basis: 375px;
+    flex-wrap: wrap;
 }
-.headline {
-    font-size: 60px;
-    font-weight: 700;
-    font-style: italic;
+.logo {
+    flex-basis: 100%;
+    width: 200px;
+    margin-bottom: 0.25rem;
 }
 .subheadline {
     margin-top: 0.25rem;
     font-size: 18px;
     font-weight: 400;
+    text-align: center;
+    margin-bottom: 0.75rem;
 }
 .button {
     position: relative;
     display: inline-block;
     margin-top: 1rem;
-    font-size: 24px;
+    font-size: 20px;
+    text-transform: uppercase;
     font-weight: 700;
     background-color: #de9316;
     border-bottom: 5px solid #815405;
     line-height: 48px;
     padding: 0 20px;
     text-decoration: none;
-    /* color: #271926; */
     color: #381c26;
     border-radius: 12px;
-    /* text-shadow: 0px 1px 2px rgba(216, 216, 216, 0.5); */
     transition: 100ms;
+    text-align: center;
 }
 .button:hover {
     background-color: #efa427;
     border-bottom-width: 3px;
     transform: translateY(2px);
+    margin-bottom: 2px;
 }
 </style>
