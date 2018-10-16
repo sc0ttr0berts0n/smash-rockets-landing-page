43cee36193c6590aaa5bd59d0ed3f3adf384398d
cyko28 <scott.t.robertson@gmail.com>
2018-10-16 02:55:50 -0400

    perfected top bar elements

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="topbar-wrapper">
-        <logo></logo>
+        <span class="warning">This game is currently in <span class="game-status">Pre-Alpha</span></span>
         <div class="social-strip">
             <a href="https://discord.gg/tzqdP69" target="_blank"><img class="social-link discord" src="../assets/social-icon-discord.svg" alt=""></a>
             <a href="https://twitter.com/smashrockets" target="_blank"><img class="social-link twitter" src="../assets/social-icon-twitter.svg" alt=""></a>
@@ -9,12 +9,8 @@
 </template>
 
 <script>
-import Logo from './Logo.vue';
 export default {
     name: 'Topbar',
-    components: {
-        Logo
-    }
 }
 </script>
 
@@ -27,25 +23,53 @@ export default {
     background-size: contain;
     background-repeat: no-repeat;
     background-position: right center;
-    height: 50px;
+    min-height: 40px;
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     align-items: center;
     padding-right: 5rem;
+    padding-top: 5px;
+    padding-bottom: 5px;
 }
 .social-link {
-    margin-left: 1rem;
-    width: 2rem;
+    margin-right: 1rem;
+    width: 1.5rem;
     height: auto;
+}
+.social-strip a:last-child .social-link {
+    margin-right: 0;
+}
+.social-strip a {
+    display: inline-block;
 }
 .discord {
-    transform: translateY(4px);
+    transform: translateY(3px);
 }
-.logo {
-    position: absolute;
-    top: 1rem;
-    left: 1rem;
-    width: 125px;
-    height: auto;
+.warning {
+    margin-left: 2rem;
+}
+.warning .game-status {
+    color: #de9316;
+}
+@media screen and (max-width: 500px) {
+    .topbar-wrapper {
+        justify-content: space-between;
+        padding-right: 4rem;
+    }
+    .social-strip {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        transform: translateY(2px);
+    }
+    .social-link {
+        margin-right: 0.75rem;
+    }
+    .discord {
+        transform: translateY(1px);
+    }
+    .warning {
+        font-size: 0.75rem;
+    }
 }
 </style>
